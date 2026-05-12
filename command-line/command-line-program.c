f5f49d1bc26daee01f928ffd3727969d7f4710ea
Austin Reilly <austin@wearejsa.com>
2026-05-11 17:50:23 -0700

    silly formatting

--- command-line/command-line-program.c
+++ command-line/command-line-program.c
@@ -3,40 +3,28 @@
 #include <signal.h>
 #include <unistd.h>
 #include <termios.h>
-
 volatile sig_atomic_t stop = 0;
-
 struct termios oldt;
-
-void restore_terminal() {
+void restore_terminal(){
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 }
-
-void handle_sigint(int sig) {
+void handle_sigint(int sig){
     stop = 1;
 }
-
-int main() {
+int main(){
     signal(SIGINT, handle_sigint);
-
     // Get current terminal settings
     tcgetattr(STDIN_FILENO, &oldt);
-
     // Copy settings and disable ^C echo
     struct termios newt = oldt;
     newt.c_lflag &= ~ECHOCTL;
-
     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-
     // Restore terminal when program exits
     atexit(restore_terminal);
-
-    while (!stop) {
+    while(!stop){
         printf("Running...\n");
         sleep(1);
     }
-
     printf("Closing program...\n");
-
     return 0;
 }
