6f4565750d8c6688a6e40a2624fb0cf69025ef12
Austin Reilly <austin@decorpo.co>
2026-06-17 12:04:02 -0700

    feat(local): fix local x.x

---
+++ local/fix-local-terminal-arch.sh
@@ -0,0 +1,8 @@
+# ls /usr/bin/gnome-terminal
+# /usr/bin/gnome-terminal
+# cat /usr/bin/gnome-terminal
+#!/bin/bash
+# exec kgx -- bash -lc "$@"
+
+# /home/user/.config/Local/lightning-services/php-8.4.18+1/bin/linux/shared-libs
+mv libreadline.so.8 libreadline.so.8.bak
